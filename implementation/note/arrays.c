19bdd17edd90c84cf9e6d205fb7413b790b717c7
Desmond Chen <desmond.chen@arm.com>
2019-05-16 15:27:47 +0800

    update stack_array, data type is char

--- implementation/note/arrays.c
+++ implementation/note/arrays.c
@@ -10,3 +10,27 @@ void EvenOdd(int32_t* array, int32_t size_of_array) {
         }
     }
 }
+
+/* 6.1 Dutch Flag Partition*/
+typedef enum {
+    RED,
+    WHITE,
+    BLUE
+} Color;
+void DutchFlagPartition(int32_t pivot_index, Color* A_ptr) {
+    Color* A = A_ptr;
+    Color pivot = A[pivot_index];
+
+    int32_t smaller = 0, equal = 0, larger = A.size();
+    while (equal < larger) {
+        if (A[equal] < pivot) {
+            swap(A[smaller++], A[equal++]);
+        } else if (A[equal] == pivot) {
+            ++equal;
+        } else {
+            swap(A[equal++], A[--larger]);
+        }
+    }
+}
+
+/* 6.2 Increment an arbitrary-precision integer */
--- implementation/stack/stack_array.c
+++ implementation/stack/stack_array.c
@@ -1,17 +1,14 @@
-
 typedef struct {
     int32_t capacity;
     int32_t top;
-    int32_t* data;
+    char* data;
 } stack;
 
-stack* newStack() {
-    const int32_t size_of_capacity = 5;
-
+stack* newStack(const int32_t size_of_capacity) {
     stack* stk = (stack*)malloc(sizeof(stack));
     stk->capacity = size_of_capacity;
     stk->top = -1;
-    stk->data = (int32_t*)malloc(stk->capacity * sizeof(int32_t));
+    stk->data = (char*)malloc(stk->capacity * sizeof(char));
 
     return stk;
 }
@@ -21,29 +18,44 @@ void freeStack(stack* stk) {
     free(stk);
 }
 
-bool_t DoubleCapacity(stack* stk) {
-    return false;
+bool DoubleCapacity(stack* stk) {
+    if (stk->capacity == 0)
+        stk->capacity = 1;
+    else
+        stk->capacity *= 2;
+
+    char* ptr_new = realloc(stk->data, stk->capacity * sizeof(char));
+    stk->data = ptr_new;
+    if (stk->data)
+        return true;
+    else
+        return false;
 }
 
-int32_t StackPop(stack* stk) {
-    int32_t data = stk->data[top];
-    --top;
+char StackPop(stack* stk) {
+    if (stk->top == -1) {
+        assert("[Error] pop an empty queue.\n");
+    }
+    char data = stk->data[stk->top];
+    --stk->top;
     return data;
 }
 
-int32_t StackPeek(stack* stk) {
-    return stk->data[top];
+char StackPeek(stack* stk) {
+    return stk->data[stk->top];
 }
 
-void StackPush(int32_ data) {
-    ++top;
-    stk->data[top] = data;
+void StackPush(stack* stk, char data) {
+    if (stk->top == stk->capacity -1) {
+        if (!DoubleCapacity(stk)) {
+            assert("overflow");
+        }
+    }
+
+    ++stk->top;
+    stk->data[stk->top] = data;
 }
 
-bool_t StackEmpty() {
-    return (top == -1);
-}
-
-bool_t ForTest() {
-    return 1;
+bool StackEmpty(stack* stk) {
+    return (stk->top == -1);
 }
